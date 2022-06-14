c45dd0f491c88209cca61ae0606d445d2c4092a6
10Rup <rupmandal.lspl@gmail.com>
2022-06-14 13:49:45 +0530

    sleecting data and group by for table

--- dashboard2/dashboard/dashboard_1.R
+++ dashboard2/dashboard/dashboard_1.R
@@ -6,31 +6,100 @@ library(shinydashboard)
 library(readxl)
 library(dplyr)
 
-
-# read.csv(file$datapath, header = input$header)
+file <- "C:/RUPMANDAL/r project/shiny project/Shiny Practice/dashboard2/NIFT_Data.xlsx"
+df <-read_excel(file)
+# names(df)
+# names(select_if(df,is.numeric))
+# nums <- unlist(lapply(df, is.numeric), use.names = FALSE) 
+# nums
 # target <- c("Tom", "Lynn")
 # target
 
+# nums <- names(select_if(df, is.numeric))
+# nums
+# hs <- "Location"
+# v1 <- df%>%group_by(hs)%>%
+#   summarise_at(vars(nums), list(sum))
+# v1
 
-ui <- fluidPage(
-  sidebarLayout(
-    sidebarPanel(
-      fileInput("file1","Choose cvs file", accept=".xlsx")
+ui <-dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody(
+    fluidRow(
+      box(
+        title = "Title 3", width = "3", 
+        solidHeader = TRUE, 
+        status = "success",
+        collapsible = TRUE,
+        collapsed = TRUE,
+        fileInput("file1","Choose cvs file", accept=".xlsx"),
+        selectInput("Colm", label = "Group by", choices =NULL ),
+        selectInput("col2", label = "Summarize", multiple = TRUE, choices =NULL ),
+        textOutput("x1_value")
+      ),
+      box(
+        tableOutput("table1")
+       
+      )
+      
     ),
-    mainPanel(
-      textOutput("text1"),
-      tableOutput("table1")
+    fluidRow(
+      
     )
+    
   )
-  
 )
 
 server <- function(input, output, session) {
-  file_name <- reactive({
-    file = input$file1
-    file$datapath
-    # file <- tools::file_path_as_absolute(file$datapath)
+  # file_name <- reactive({
+  #   file = input$file1
+  #   file$datapath
+  #   # file <- tools::file_path_as_absolute(file$datapath)
+  # })
+  
+  data_source<- reactive({
+    file <- input$file1
+    
+    req(file)
+    read_excel(file$datapath)
+    
   })
+  
+  
+  
+  
+  observeEvent(data_source(),{
+    choices <- names(data_source()) 
+    updateSelectInput(inputId = "Colm", choices = c("All",choices))
+  })
+  
+  observeEvent(data_source(),{
+    choices <- names(select_if(data_source(), is.numeric))
+    updateSelectInput(inputId = "col2", choices = c("All",choices))
+  })
+  
+  output$x1_value<- renderText({
+    input$col2
+  })
+  
+  
+  output$table1<- renderTable({
+    # req(input$Colm)
+    # req(input$col2)
+    
+    
+    df<- data_source()%>%group_by(input$Colm)%>%
+      summarise_at(vars(input$col2),list(sum))
+    df
+  })
+  
+  
+  
+  # col2_data<- reactive({
+  #   req(input$colms)
+  # })
+  # 
   
   
   output$text1 <- renderText({
@@ -41,16 +110,16 @@ server <- function(input, output, session) {
     # file <- tools::file_path_as_absolute(file$datapath)
   })
   
-  output$table1 <- renderTable({
-    file <- input$file1
-    ext <-tools::file_ext(file$datapath)
-    
-    req(file)
-    validate(need(ext == "xlsx", "upload a csv file"))
-    
-    read_excel(file$datapath)
-    
-  })
+  # output$table1 <- renderTable({
+  #   file <- input$file1
+  #   ext <-tools::file_ext(file$datapath)
+  #   
+  #   req(file)
+  #   validate(need(ext == "xlsx", "upload a csv file"))
+  #   
+  #   read_excel(file$datapath)
+  #   
+  # })
   
   
 }
---
+++ dashboard2/dashboard/experiments.R
@@ -0,0 +1,86 @@
+library(shiny)
+
+# ui <- fluidPage(
+#   numericInput("min", "Minimum", 0),
+#   numericInput("max", "Maximum", 3),
+#   sliderInput("n", "n", min = 0, max = 3, value = 1)
+# )
+# server <- function(input, output, session) {
+#   observeEvent(input$min, {
+#     updateSliderInput(inputId = "n", min = input$min)
+#   })  
+#   observeEvent(input$max, {
+#     updateSliderInput(inputId = "n", max = input$max)
+#   })
+# }
+
+
+# ui <- fluidPage(
+#   sliderInput("x1", "x1", 0, min = -10, max = 10),
+#   sliderInput("x2", "x2", 0, min = -10, max = 10),
+#   sliderInput("x3", "x3", 0, min = -10, max = 10),
+#   actionButton("reset", "Reset")
+# )
+# 
+# server <- function(input, output, session) {
+#   observeEvent(input$reset, {
+#     updateSliderInput(inputId = "x1", value = 0)
+#     updateSliderInput(inputId = "x2", value = 0)
+#     updateSliderInput(inputId = "x3", value = 0)
+#   })
+# }
+
+
+
+sales <- vroom::vroom("sales_data_sample.csv", col_types = list(), na = "")
+sales %>% 
+  select(TERRITORY, CUSTOMERNAME, ORDERNUMBER, everything()) %>%
+  arrange(ORDERNUMBER)
+
+
+
+ui <- fluidPage(
+  selectInput("territory", "Territory", choices = unique(sales$TERRITORY)),
+  selectInput("customername", "Customer", choices = NULL),
+  selectInput("ordernumber", "Order number", choices = NULL),
+  tableOutput("data")
+)
+
+server <- function(input, output, session) {
+  territory <- reactive({
+    filter(sales, TERRITORY == input$territory)
+  })
+  observeEvent(territory(), {
+    choices <- unique(territory()$CUSTOMERNAME)
+    updateSelectInput(inputId = "customername", choices = choices) 
+  })
+  
+  customer <- reactive({
+    req(input$customername)
+    filter(territory(), CUSTOMERNAME == input$customername)
+  })
+  observeEvent(customer(), {
+    choices <- unique(customer()$ORDERNUMBER)
+    updateSelectInput(inputId = "ordernumber", choices = choices)
+  })
+  
+  output$data <- renderTable({
+    req(input$ordernumber)
+    customer() %>% 
+      filter(ORDERNUMBER == input$ordernumber) %>% 
+      select(QUANTITYORDERED, PRICEEACH, PRODUCTCODE)
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+shinyApp(ui, server)
