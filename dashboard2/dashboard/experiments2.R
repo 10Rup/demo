571e9528daaaed7832a4e197d61545831b03e0f7
10Rup <rupmandal.lspl@gmail.com>
2022-06-23 19:00:50 +0530

    alerts

--- dashboard2/dashboard/experiments2.R
+++ dashboard2/dashboard/experiments2.R
@@ -49,28 +49,89 @@ library(shinyjs)
 # install.packages("shinyalert")
 library(shinyalert)
 
+# ui <- fluidPage(
+#   useShinyalert()
+# )
+# 
+# server <- function(input, output) {
+#   shinyalert(
+#     title = "Hello",
+#     text = "This is a modal",
+#     size = "s", 
+#     closeOnEsc = TRUE,
+#     closeOnClickOutside = FALSE,
+#     html = FALSE,
+#     type = "success",
+#     showConfirmButton = TRUE,
+#     showCancelButton = FALSE,
+#     confirmButtonText = "OK",
+#     confirmButtonCol = "#AEDEF4",
+#     timer = 0,
+#     imageUrl = "",
+#     animation = TRUE
+#   )
+# }
+# 
+# 
+
+
+
+
+# ui <- fluidPage(
+#   textInput("viewname", "VIEW NAME"),
+#   actionButton("preview", "Preview")
+# )
+# 
+# server <- function(input, output, session) {
+#   
+#   condition<- reactive({
+#     paste0("1")
+#   })
+#   
+#   observeEvent(input$preview, {
+#     # Show a modal when the button is pressed
+#     req(input$viewname)
+#     shinyalert("Oops!", "Something went wrong.", type = "error")
+#   })
+# }
+
+
+# demo
+# listOfAnns <- list("demo"," dadsdsdemo", "dsd", "sasd")
+# listOfAnns
+# 
+# 
+identical(listOfAnns,'demo', ignore.bytecode=FALSE)
+VIEW_PATH <-"C:/RUPMANDAL/r project/demo/dashboard2/dashboard/views/Views.xlsx"
+views<-readWorkbook(VIEW_PATH)
+views$view_name
+views["view_name"]
+# 
+# "demno" %in% list(views["view_name"])
+# identical(views["view_name"],'demo')
+# 
+# "a" %in% "a"
+a<-c(unique(views$view_name))
+a
+"demo" %in% a
+indentical(a,"demo")
+
+
 ui <- fluidPage(
-  useShinyalert()
+  checkboxGroupInput('in1', 'Check some letters', choices = head(LETTERS)),
+  selectizeInput('in2', 'Select a state', choices = c("", state.name)),
+  plotOutput('plot')
 )
 
 server <- function(input, output) {
-  shinyalert(
-    title = "Hello",
-    text = "This is a modal",
-    size = "s", 
-    closeOnEsc = TRUE,
-    closeOnClickOutside = FALSE,
-    html = FALSE,
-    type = "success",
-    showConfirmButton = TRUE,
-    showCancelButton = FALSE,
-    confirmButtonText = "OK",
-    confirmButtonCol = "#AEDEF4",
-    timer = 0,
-    imageUrl = "",
-    animation = TRUE
-  )
+  output$plot <- renderPlot({
+    validate(
+      need(input$in1, 'Check at least one letter!'),
+      need(input$in2 != '', 'Please choose a state.')
+    )
+    plot(1:10, main = paste(c(input$in1, input$in2), collapse = ', '))
+  })
 }
 
-
 shinyApp(ui, server)
+
--- dashboard2/dashboard/reactive_server.R
+++ dashboard2/dashboard/reactive_server.R
@@ -3,9 +3,34 @@ VIEW_PATH <-"C:/RUPMANDAL/r project/demo/dashboard2/dashboard/views/Views.xlsx"
 
 # server codes
 rs.server<- function(input, output, session) {
-  # output$name <- renderText({
-  #   paste0(input$viewname)
-  # })
+  output$name <- renderText({
+    # views<-readWorkbook(VIEW_PATH)
+    # existing_views<-c(unique(views$view_name))
+    # validate(
+    # need(input$viewname %in% existing_views == FALSE,"Already Exists")
+    # )
+    paste0(input$viewname)
+  })
+  
+  observeEvent(input$viewname,{
+    
+    req(input$viewname)
+    # validate(
+    #   need(input$viewname %in% existing_views == FALSE,
+    #        shinyalert(
+    #          "Already Exists!",
+    #          "",
+    #          type = "warning",
+    #          showConfirmButton = TRUE,))
+    # )
+    views<-readWorkbook(VIEW_PATH)
+    existing_views<-c(unique(views$view_name))
+    validate(
+      need(input$viewname %in% existing_views == FALSE,"Already Exists")
+    )
+
+  })
+  
   
   data_source<- reactive({
     file <- input$reportfile
@@ -32,34 +57,40 @@ rs.server<- function(input, output, session) {
   df<-data.frame(view_name,group,sumrz)
   })
   
-  on_click_create<-reactive({
-    shinyalert(
-      title = "Hello",
-      text = "This is a modal",
-      size = "s", 
-      closeOnEsc = TRUE,
-      closeOnClickOutside = FALSE,
-      html = FALSE,
-      type = "success",
-      showConfirmButton = TRUE,
-      showCancelButton = FALSE,
-      confirmButtonText = "OK",
-      confirmButtonCol = "#AEDEF4",
-      timer = 0,
-      imageUrl = "",
-      animation = TRUE
-    )
-  })
+  # on_click_create<-reactive({
+  #   shinyalert(
+  #     title = "Hello",
+  #     text = "This is a modal",
+  #     size = "s",
+  #     closeOnEsc = TRUE,
+  #     closeOnClickOutside = FALSE,
+  #     html = FALSE,
+  #     type = "success",
+  #     showConfirmButton = TRUE,
+  #     showCancelButton = FALSE,
+  #     confirmButtonText = "OK",
+  #     confirmButtonCol = "#AEDEF4",
+  #     timer = 0,
+  #     imageUrl = "",
+  #     animation = TRUE
+  #   )
+  # })
   
   observeEvent(input$create,{
-    req(new_data())
+    # req(input$reportfile)
+    req(input$viewname,input$reportfile)
+    
     cvp.append_func(VIEW_PATH,new_data())
-    # req(new_data())
-    # on_click_create()
+    shinyalert(
+      "Updated!",
+      "View Created", 
+      type = "success",
+      showConfirmButton = FALSE,)
+    
   })
   
   # observeEvent({})
   # onclick(id = "create",alert("Data Updated"))
-  onclick(id = "create",on_click_create())
+  # onclick(id = "create",on_click_create())
 
 }
